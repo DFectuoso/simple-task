52cafc109e23fb26c10001170bf6f3a0c278b6f9
Santiago <santiago1717@gmail.com>
2012-08-26 00:24:27 -0500

    delay 1

--- task.py
+++ task.py
@@ -6,6 +6,7 @@ import urllib2
 random.seed()
 
 while True:
+    time.sleep(1)
     req = urllib2.Request('http://dfectuoso.com/')
     response = urllib2.urlopen(req)
     the_page = response.read()    
